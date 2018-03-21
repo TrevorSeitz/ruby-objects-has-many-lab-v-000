a586e80a3cfa81b0516f6d079dc7789358ec8bab
Trevor Seitz <trevinla@gmail.com>
2018-03-21 15:03:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 class Song
-  attr_accessor :artist, :name, :genre
+  attr_accessor :artist = nil, :name, :genre
 
   def initialize(name)
     @name = name
     @genre = genre
   end
+
+  def artist(name)
+    @artist = artist
+  end
+
 end
